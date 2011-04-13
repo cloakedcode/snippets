fdf17cf4487662bab16754dc1b237998a53b8fd3
Alan Smith <alan@cloakedcode.com>
2011-04-13 17:49:00 -0600

    Added stamp_pdf

--- stamp_pdf/pdf.php
+++ stamp_pdf/pdf.php
@@ -8,12 +8,10 @@ class PDF extends FPDI
 	function Header() {}
 	function Footer() {}
 
-	static function stampPDFPages($pdf_file, $img_file)
+	static function StampPDFPages($pdf_file, $img_file)
 	{
 		$p = new self();
 		$count = $p->setSourceFile($pdf_file);
-
-		$imgsize = getimagesize($img_file);
 
 		for ($i = 1; $i <= $count; $i++)
 		{
@@ -30,7 +28,7 @@ class PDF extends FPDI
 	}
 }
 
-$pdf = PDF::stampPDFPages('test.pdf', 'test.png');
+$pdf = PDF::StampPDFPages('test.pdf', 'test.png');
 $pdf->Output('stamp.pdf', 'F');
 
 ?>
